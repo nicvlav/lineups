e53a68c5976c2421a430630f08b77b6b77f67e4c
nicvlav <nicolasvlavianos@gmail.com>
2025-02-16 01:16:35 -0800

    Added device detection

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
+import { isMobile } from 'react-device-detect'; // Use this to detect touch devices
 import { PlayersProvider } from "./components/global/PlayersContext.jsx";
+
 import CurrentGame from "./components/game/CurrentGame.jsx";
 import Layout from "./components/Layout.jsx";
 import React from "react";
 
 const App = () => {
+  const backend = isMobile ? TouchBackend : HTML5Backend;
+
   return (
-    <PlayersProvider>
-      <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={backend}>
+      <PlayersProvider>
         <Layout>
-          {/* Ensure this div fills the entire screen */}
-          {/* <div className="h-full w-full flex"> */}
-            <CurrentGame className="flex-1" />
-          {/* </div> */}
+          <CurrentGame className="flex-1" />
         </Layout>
-      </DndProvider>
-    </PlayersProvider>
+      </PlayersProvider>
+    </DndProvider>
   );
 };
-
 
 export default App;
